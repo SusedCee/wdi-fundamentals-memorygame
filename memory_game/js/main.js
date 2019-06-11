92badc7d06bdfd7ff5f905d5ad81f547f8e32d85
Sused Cabrera <susedcabrera@suseds-mbp.attlocal.net>
2019-06-10 18:50:29 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,19 +2,29 @@ console.log("Up and running!");
 
 window.confirm("WARNING! You're about to play a really cool game!")
 
-var cards = ["queen", "queen", "king", "king"]
-var cardsInPlay = []
 
-var cardOne = ["queen"]
-cardOne.push('queen')
-console.log("User flipped queen")
+var cardOne = ["queen"];
+cardOne.push('queen');
+console.log("User flipped queen");
 
-var cardTwo = ["king"]
-cardTwo.push('queen')
-console.log("User flipped king")
+var cardTwo = ["king"];
+cardTwo.push('king');
+console.log("User flipped king");
 
-if (cardsInPlay < 2) [
-	cardsInPlay === 2]
+//var cardThree = []
+
+//if (cardsInPlay < 2) [
+//	cardsInPlay === 2]
+var cardsInPlay = [];
+
+if (cardsInPlay.length === 2) {
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+    alert("You found a match!");
+} else {
+    alert("Sorry, try again.")
+} 
+} 
 
 
 let cards = [
@@ -61,11 +71,45 @@ let cards = [
 }];
 
 
-var score = 0;
-var resetButton = document.getElementById('reset');
-var statusMessage = document.getElementbyId('status-message');
-var flipCount = 0;
 
-var createBoard = 
+ function checkForMatch() {
+    if (cardsInPlay[0] === cardsInPlay[1]) {
+          alert("You found a match!");
+      } else {
+          alert("Sorry, try again");
+        };
+  };
+
+function flipCard() {
+    var cardId = this.getAttribute('data-id');
+  	cardsInPlay.push(cards[cardId].rank);
+ 	this.setAttribute('src', cards[cardId].cardImage);
+ 	if (cardsInPlay.length === 2){
+    checkForMatch();
+ };
+};
+
+console.log(cardsInPlay);
+
+function createBoard() {
+  	const cardTable = document.getElementById('game-board');
+  	for (var i = 0; i < cards.length; i++) {
+  	var cardElement = document.createElement('img');
+    cardElement.setAttribute('src', 'images/back.png');
+    cardElement.setAttribute('data-id', i);
+    cardElement.addEventListener('click', flipCard);
+    cardTable.appendChild(cardElement);
+  }
+};
+
+
+//var score = 0;
+//var resetButton = document.getElementById('reset');
+//var statusMessage = document.getElementbyId('status-message');
+//var flipCount = 0;
+
+var createBoard = function() {
+
+}
 
 
