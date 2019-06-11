8a49d182d2cea3dc1a81abcd9853a6db12b4425e
Sused Cabrera <susedcabrera@suseds-mbp.attlocal.net>
2019-06-10 17:22:09 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,6 +1,71 @@
 console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-console.log("User flipped" + cardOne);
+
+window.confirm("WARNING! You're about to play a really cool game!")
+
+var cards = ["queen", "queen", "king", "king"]
+var cardsInPlay = []
+
+var cardOne = ["queen"]
+cardOne.push('queen')
+console.log("User flipped queen")
+
+var cardTwo = ["king"]
+cardTwo.push('queen')
+console.log("User flipped king")
+
+if (cardsInPlay < 2) [
+	cardsInPlay === 2]
+
+
+let cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+},
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png"
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}];
+
+
+var score = 0;
+var resetButton = document.getElementById('reset');
+var statusMessage = document.getElementbyId('status-message');
+var flipCount = 0;
+
+var createBoard = 
+
+
