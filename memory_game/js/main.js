2370bced9b9e6f50f66574ed2968f2c138a82029
Sused Cabrera <susedcabrera@Suseds-MacBook-Pro.local>
2019-06-14 13:35:28 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -2,25 +2,20 @@ console.log("Up and running!");
 
 window.confirm("WARNING! You're about to play a really cool game!")
 
-var cardOne = "queen";
-var cardTwo = "queen";
-var cardThree = "king";
-var cardFour = "king";
-
 console.log("User flipped" + cardOne);
+console.log("User flipped" + cardTwo);
 
+var cards = ["queen", "queen", "king", "king"];
+var cardsInPlay = [];
 
+var cardOne = cards[0];
+cardsInPlay.push("queen");
+console.log("User flipped queen");
 
+var cardTwo = cards[3];
+cardsInPlay.push("king");
+console.log("User flipped king");
 
+if (cardsInPlay.length === 2) {
 
-
-
-
-
-
-
-
-
-
-
-
+}
